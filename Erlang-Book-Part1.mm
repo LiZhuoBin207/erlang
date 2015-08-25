5ee829ee4993bfeb64f43ca064d0d4865f6f004f
lizhuobin <lzb383551179@gmail.com>
2015-08-25 14:36:12 +0800

    Signed-off-by: lizhuobin <lzb383551179@gmail.com>

---
+++ Erlang-Book-Part1.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1440484484584" ID="ID_677223958" MODIFIED="1440484535852" TEXT="Erlang-Book-Part1"/>
+</map>
